ca0d91e2a6f9ae801ef5e8817cddf7ec954023f6
SvenSven <mikorecare@gmail.com>
2023-11-11 15:43:04 +0800

    FEAT: Added header and footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  status = false;
+    addToggle(){
+      this.status = !this.status;       
+    }
 
 }
