3af9060bc92500ec456695d8939ddb526fb5c506
SvenSven <mikorecare@gmail.com>
2023-11-15 08:18:17 +0800

    FEAT: using directive to loop the skills card

--- src/app/components/home/skills/skills.component.ts
+++ src/app/components/home/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Profile } from 'src/app/data/data';
 
 @Component({
   selector: 'app-skills',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+  skills:any[] = new Profile().skills;
+  finalSkills:any[] = []
+  ngOnInit():void{
+     this.finalSkills = this.skills.map((e)=>{
 
+      let logoName = `logo-${e.name}`
+      return {...e,"logo":logoName.toLocaleLowerCase()}
+    })
+    console.log(this.finalSkills);
+  }
 }
