5778e4a57a94960dcd7d9e80574a169251893258
SvenSven <mikorecare@gmail.com>
2023-11-15 11:04:17 +0800

    FEAT: Added skill icon logic

--- src/app/components/home/skills/skills.component.ts
+++ src/app/components/home/skills/skills.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Profile, ionicIcons } from 'src/app/data/data';
+import { Profile, ionicIcons, specialIcons } from 'src/app/data/data';
+import { Skills } from 'src/app/data/interfaces';
 
 @Component({
   selector: 'app-skills',
@@ -10,14 +11,17 @@ export class SkillsComponent {
   skills:any[] = new Profile().skills;
   finalSkills:any[] = []
   ngOnInit():void{
-     this.finalSkills = this.skills.map((e)=>{
+     this.finalSkills = this.skills.map((e:Skills)=>{
       let logoName = `logo-${e.name}`
       if(ionicIcons.includes(e.name.toLocaleLowerCase())) 
         {
           return {...e,"logo":logoName.toLocaleLowerCase(),"type":"ionic"}
         }
+      else if(specialIcons.includes(e.name.toLocaleLowerCase())){
+        return {...e,"logo":'assets/images/'+e.name.toLocaleLowerCase()+'.svg',"type":"special"}
+      }
       else{
-        return {...e,"logo":logoName.toLocaleLowerCase(),"type":"none"}
+        return {...e,"logo":e.name.toLocaleLowerCase(),"type":"none"}
       }
     })
     console.log(this.finalSkills)
