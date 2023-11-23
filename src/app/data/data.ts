9fadae626dfb1788b436f67604a74e217fee95af
SvenSven <mikorecare@gmail.com>
2023-11-23 10:50:26 +0800

    FEAT: Last update

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -55,7 +55,8 @@ export const specialIcons = [
   "mongoose",
   "mysql",
   "express",
-  "postman"
+  "postman",
+  "typescript"
 ]
 export class Profile implements ProfileInfo{
     firstName = "Miko"
@@ -74,6 +75,7 @@ export class Profile implements ProfileInfo{
       {name:"HTML5",level:5},
       {name:"CSS3",level:5},
       {name:"Javascript",level:5},
+      {name:"Typescript",level:5},
       {name:"Bootstrap",level:5},
       {name:"Github",level:4},
       {name:"AWS",level:3.5},
@@ -82,15 +84,75 @@ export class Profile implements ProfileInfo{
     ]
     certificates = [
       {
-        name: "Test Certification",
-        description:"This is a test description",
-        image: "assets/images/gb.png",
-        url:"https://google.com",
-        provider:"Test Provider",
+        name: "Learning MEAN Stack by Building Real world Application Specialization",
+        description:"Build fully functional web apps using MEAN stack. Acquire comprehensive skills in MongoDB, Express.js, Angular, and Node.js to design, develop, and deploy real-world high-performance web applications. ",
+        image: "assets/images/HY6U4NZB6LT3.jpg",
+        url:"https://www.coursera.org/account/accomplishments/specialization/certificate/HY6U4NZB6LT3",
+        provider:"Coursera",
+        date_earned:new Date("11-15-23"),
+        expiration:new Date("11-23-23"),
+        credential_id:"HY6U4NZB6LT3"
+      },
+      {
+        name: "Building RESTful APIs with Node.js and Express",
+        description:"This course is part of the Learning MEAN Stack by Building Real world Application Specialization",
+        image: "assets/images/F6PG6WR8LDMS.jpg",
+        url:"https://www.coursera.org/account/accomplishments/certificate/F6PG6WR8LDMS",
+        provider:"Coursera",
         date_earned:new Date("11-4-23"),
         expiration:new Date("11-23-23"),
-        credential_id:"Test Credential ID"
-      }
+        credential_id:"F6PG6WR8LDMS"
+      },
+      {
+        name: "Frontend Development using Angular",
+        description:"This course is part of the Learning MEAN Stack by Building Real world Application Specialization",
+        image: "assets/images/RBPN4P2E9QRZ.jpg",
+        url:"https://www.coursera.org/account/accomplishments/certificate/RBPN4P2E9QRZ",
+        provider:"Coursera",
+        date_earned:new Date("11-4-23"),
+        expiration:new Date("11-23-23"),
+        credential_id:"RBPN4P2E9QRZ"
+      },
+      {
+        name: "Building a Complete MEAN Stack Application",
+        description:"This course is part of the Learning MEAN Stack by Building Real world Application Specialization",
+        image: "assets/images/5ZA85SY9NUWP.jpg",
+        url:"https://www.coursera.org/account/accomplishments/certificate/5ZA85SY9NUWP",
+        provider:"Coursera",
+        date_earned:new Date("11-4-23"),
+        expiration:new Date("11-23-23"),
+        credential_id:"5ZA85SY9NUWP"
+      },
+      {
+        name: " Angular7 for Advanced Level",
+        description:"Hi all, I have successfully completed the \" Angular7 for Advanced Level\" course offered by Great Learning Academy. #GreatLearningAcademy #greatlearning #glacertificate",
+        image: "assets/images/ZWGBTPCZ.jpg",
+        url:"https://verify.mygreatlearning.com/verify/ZWGBTPCZ",
+        provider:"Great Learning",
+        date_earned:new Date("10-4-23"),
+        expiration:new Date("11-23-23"),
+        credential_id:"ZWGBTPCZ"
+      },
+      {
+        name: " Angular7 for Intermediate Level",
+        description:"Hi all, I have successfully completed the \" Angular7 for Intermediate Level\" course offered by Great Learning Academy. #GreatLearningAcademy #greatlearning #glacertificate",
+        image: "assets/images/AMEQPVSD.jpg",
+        url:"https://verify.mygreatlearning.com/verify/AMEQPVSD",
+        provider:"Great Learning",
+        date_earned:new Date("10-4-23"),
+        expiration:new Date("11-23-23"),
+        credential_id:"AMEQPVSD"
+      },
+      {
+        name: " Angular7 for Beginners",
+        description:"Hi all, I have successfully completed the \" Angular7 for Beginners\" course offered by Great Learning Academy. #GreatLearningAcademy #greatlearning #glacertificate",
+        image: "assets/images/IWCUSLNR.jpg",
+        url:"https://verify.mygreatlearning.com/verify/IWCUSLNR",
+        provider:"Great Learning",
+        date_earned:new Date("10-4-23"),
+        expiration:new Date("11-23-23"),
+        credential_id:"IWCUSLNR"
+      },
     ]
     yearsOfExperience = 2;    
 }
